010e6593bebe84b1795d8624d7b72e37c1612d76
Daryl Sabado <sabado114daryl@gmail.com>
2025-04-14 08:34:48 +0800

    2nd commit

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -80,7 +80,7 @@ const loginCandidate = async (req, res) => {
     res
       .cookie("token", token, {
         httpOnly: true,
-        secure: process.env.NODE.ENV === "production", // set true in production
+        secure: process.env.NODE_ENV === "production", // set true in production
         sameSite: "Strict", // or "Lax"
         maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
       })
